714d2896d81f3374953866f1982582b2d0cae574
MemoSy <ahmeddalhalabi1@gmail.com>
2023-08-16 17:38:13 +0300

    a

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -31,7 +31,7 @@ export default function Navbar() {
                 {/* Mobile menu button*/}
                 <Disclosure.Button className="relative inline-flex items-center justify-center rounded-md p-2 text-gray-400 hover:bg-gray-700 hover:text-white focus:outline-none focus:ring-2 focus:ring-inset focus:ring-white">
                   <span className="absolute -inset-0.5" />
-                  {/* <span className="sr-only">Open main menu</span> */}
+                  <span className="sr-only">Open main menu</span>
                   {open ? (
                     <XMarkIcon className="block h-6 w-6" aria-hidden="true" />
                   ) : (
