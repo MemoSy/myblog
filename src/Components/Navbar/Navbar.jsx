34699b636261eb13fbe3c7f075a8723833b8d7e8
MemoSy <ahmeddalhalabi1@gmail.com>
2023-08-21 16:53:59 +0300

    headerupdate

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -23,37 +23,16 @@ function classNames(...classes) {
 export default function Navbar() {
   const session = useSession()
   return (
-    <div class="nav">
-      <input type="checkbox" id="nav-check" />
-      <div class="nav-header">
-        <div class="nav-title">
-          JoGeek
-        </div>
-      </div>
-      <div class="nav-btn">
-        <label for="nav-check">
-          <span></span>
-          <span></span>
-          <span></span>
-        </label>
-      </div>
-      
-      <div class="nav-links">
+    <div>
         <DarkModeToggle />
         {navigation.map((item) => (
-          <Link
-            key={item.name}
-            href={item.href}
-            className={classNames(
-              item.current ? 'bg-gray-900 text-white' : 'text-gray-300 hover:bg-gray-700 hover:text-white',
-              'rounded-md px-3 py-2 text-sm font-medium'
-            )}
+          <li
+            class="center"
             aria-current={item.current ? 'page' : undefined}
           >
-            {item.name}
-          </Link>
+            <Link href={item.href}> {item.name} </Link>
+          </li>
         ))}
-      </div>
     </div>
   )
 }
